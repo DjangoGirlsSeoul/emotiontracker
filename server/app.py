e62a45abef68c27ff7ab16cda705a82aadacf5d0
mojosoeun <mojosoeun@gmail.com>
2016-08-27 22:16:37 +0900

    feat: move song list code to index.htm

--- server/app.py
+++ server/app.py
@@ -1,6 +1,5 @@
 from flask import Flask, request, render_template, send_from_directory
 from werkzeug.contrib.cache import SimpleCache
-from game import Game
 
 # songs = [{"speed" : 1, "notes" : ['a','b','c','g'] }, {"speed" : 1, "notes" : ['b','c','c','g'] }]
 WEBCAM_SPEED = 1
@@ -11,12 +10,12 @@ cache.set('webcam_speed', 1)
 
 @app.route('/')
 def main():
-    g = Game()
-    g.run()
+    # g = Game()
+    # g.run()
     return render_template('index.htm')
 
 ##{'[{"emotion":"angry","value":0.03350348808431149},{"emotion":"sad","value":0.31215305776434915},{"emotion":"surprised","value":0.16222857011091235},{"emotion":"happy","value":0.04008017326503987}]': u''}
-@app.route('/emotions', methods=['POST'])
+# @app.route('/emotions', methods=['POST'])
 def get_emotions():
     emotions = request.get_json()
     happy_point = request.json[3]['value']
