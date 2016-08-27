449b40092ed9f159272496eafc0edd60d6f98645
mojosoeun <mojosoeun@gmail.com>
2016-08-27 16:20:27 +0900

    feat: make flask

--- server/app.py
+++ server/app.py
@@ -1,3 +1,4 @@
+from flask import Flask, request, render_template, send_from_directory
 import signal
 
 class AlarmException(Exception):
@@ -43,9 +44,32 @@ def play_level(song):
         print("fail")
         return False
 
-for idx,song in enumerate(songs):
-    print("level : {}".format(idx+1))
-    passing = play_level(song)
-    if not passing:
-        print("You failed!!!!!!!!!!!!")
-        exit()
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    # for idx,song in enumerate(songs):
+    #     print("level : {}".format(idx+1))
+    #     passing = play_level(song)
+    #     if not passing:
+    #         print("You failed!!!!!!!!!!!!")
+    #         exit()
+    return render_template('index.htm')
+##{'[{"emotion":"angry","value":0.03350348808431149},{"emotion":"sad","value":0.31215305776434915},{"emotion":"surprised","value":0.16222857011091235},{"emotion":"happy","value":0.04008017326503987}]': u''}
+@app.route('/emotions', methods=['POST'])
+def get_emotions():
+    emotions = request.get_json()
+    happy_point = request.json[3]['value']
+    if happy_point < 0.3
+        WEBCAM_SPEED = 2
+
+    return 'Hello World!'
+
+
+@app.route('/index_files/<path:path>')
+def send_js(path):
+    return send_from_directory('templates/index_files', path)
+
+
+if __name__ == '__main__':
+    app.run()
