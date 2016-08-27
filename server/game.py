934b00459285c525d97704b82b19d5aca6b39aea
Jin Park <jin+github@jinpark.net>
2016-08-27 17:56:48 +0900

    fix game class, ignore .pyc

--- server/game.py
+++ server/game.py
@@ -1,14 +1,18 @@
 import signal
+from audio import play_audio
+import threading
+
+WEBCAM_SPEED = 1
 
 class AlarmException(Exception):
     pass
 
 class Game:
-    def alarmHandler(signum, frame):
+    def alarmHandler(self, signum, frame):
         raise AlarmException
 
-    def nonBlockingRawInput(prompt='', timeout=5):
-        signal.signal(signal.SIGALRM, alarmHandler)
+    def nonBlockingRawInput(self, prompt='', timeout=5):
+        signal.signal(signal.SIGALRM, self.alarmHandler)
         signal.alarm(timeout)
         try:
             text = raw_input(prompt)
@@ -19,12 +23,15 @@ class Game:
         signal.signal(signal.SIGALRM, signal.SIG_IGN)
         return ''
 
-    def play_level(song):
+    def play_level(self, song):
         score = 0
         success_score = len(song["notes"]) * 0.8
         for note in song["notes"]:
             # send(note)
-            input_note = nonBlockingRawInput("note: ", note["speed"] * WEBCAM_SPEED)
+            threading.Thread(target=play_audio, kwargs={'audio_file_location': 'audio_files/ox.wav'}).start()
+            print('asdasdasasda')
+            print(note)
+            input_note = self.nonBlockingRawInput("note: ", song["speed"] * WEBCAM_SPEED)
 
             if input_note == note :
                 score += 1
@@ -38,10 +45,18 @@ class Game:
             print("fail")
             return False
 
-    def start(songs):
+    def start(self, songs):
         for idx,song in enumerate(songs):
             print("level : {}".format(idx+1))
-            passing = play_level(song)
+            passing = self.play_level(song)
             if not passing:
                 print("You failed!!!!!!!!!!!!")
                 exit()
+
+g = Game()
+g.start(
+    [
+    {"speed" : 1, "notes" : ['a','b','c','g'] }, 
+    {"speed" : 1, "notes" : ['b','c','c','g'] }
+    ]
+    )
